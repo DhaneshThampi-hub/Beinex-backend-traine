0d3d2e66497b6acecc9717adc76f7116e10b7181
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-11 17:59:05 +0530

    Update task5q13.py

--- week1/task5/task5q13.py
+++ week1/task5/task5q13.py
@@ -15,14 +15,12 @@ for i in range(1, rows + 1):
     print()
 #c
 for i in range(1, rows + 1):
-    # Inner loop for repeating numbers in each row
     for j in range(1, i + 1):
         print(i, end=" ")
     print()
 
 #d
 for i in range(1, rows + 1):
-    # Inner loop for '*' in each row
     for j in range(1, i + 1):
         print("*", end=" ")
     
