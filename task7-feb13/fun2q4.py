6478e321328d746b7ef132f32680e72273201669
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-13 13:39:52 +0530

    Update fun2q4.py

--- task7-feb13/fun2q4.py
+++ task7-feb13/fun2q4.py
@@ -10,6 +10,7 @@ from datetime import datetime
 extract_info = lambda dt: (dt.year, dt.month, dt.day, dt.strftime("%H:%M:%S"))
 current_datetime = datetime.now()
 result = extract_info(current_datetime)
+print(result)
 print("Year:", result[0])
 print("Month:", result[1])
 print("Date:", result[2])
