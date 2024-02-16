ad2cb0acdad06f748b123f8f10100cea1e102522
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-16 13:14:48 +0530

    Update dictq7.py

--- task10-feb16/dictq7.py
+++ task10-feb16/dictq7.py
@@ -1,13 +1,14 @@
 
 '''7.	Write a Python program to Delete a list of keys from a dictionary'''
 
-a=input("enter key1: ")
-b=input("enter key2: ")
-c=input("enter key3: ")
-d=input("enter a value for key1: ")
-e=input("enter a value for key2: ")
-f=input("enter a value for key3: ")
-g=input('enter a key to remove: ')
-my_dict = {a: d, b: e, c:f}
-del my_dict[g]
-print(my_dict)
+while 1:
+    a=input("enter key1: ")
+    b=input("enter key2: ")
+    c=input("enter key3: ")
+    d=input("enter a value for key1: ")
+    e=input("enter a value for key2: ")
+    f=input("enter a value for key3: ")
+    g=input('enter a key to remove: ')
+    my_dict = {a: d, b: e, c:f}
+    del my_dict[g]
+    print(my_dict)
