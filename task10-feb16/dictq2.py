eb93a2ab5d30516b67e8ae2a8cc7bcf2f597c3f1
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-16 13:11:15 +0530

    Update dictq2.py

--- task10-feb16/dictq2.py
+++ task10-feb16/dictq2.py
@@ -8,12 +8,12 @@ def keys_as_list(dictionary):
     for value in dictionary.values():
         value_list.append(value)
     return keys_list,value_list
-
-a=input("enter key1: ")
-b=input("enter key2: ")
-c=input("enter key3: ")
-d=input("enter a value for key1: ")
-e=input("enter a value for key2: ")
-f=input("enter a value for key3: ")
-dict = {a: d, b: e, c: f}
-print(keys_as_list(dict))  
+while 1:
+    a=input("enter key1: ")
+    b=input("enter key2: ")
+    c=input("enter key3: ")
+    d=input("enter a value for key1: ")
+    e=input("enter a value for key2: ")
+    f=input("enter a value for key3: ")
+    dict = {a: d, b: e, c: f}
+    print(keys_as_list(dict)) 
