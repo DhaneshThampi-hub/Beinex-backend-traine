f2c637eb833863128395295001356df518bf862b
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-16 13:04:19 +0530

    Update dictq1.py

--- task10-feb16/dictq1.py
+++ task10-feb16/dictq1.py
@@ -8,17 +8,21 @@ def merge_dicts(dict1, dict2, merge='1'):
     else:
         raise ValueError("Invalid merge strategy. Choose 'first' or 'second'.")
     return merged_dict
-a=input("enter a key1 for dict1: ")
-b=input("enter a key2 for dict1: ")
-c=input("enter a value for dict1: ")
-d=input("enter a value for dict1: ")
-e=input("enter a key1 for dict2: ")
-f=input("enter a key2 for dict2: ")
-g=input("enter a value for key2: ")
-h=input("enter a value for key2: ")
-dict1 = {a: c, b: d}
-dict2 = {e: g, f: h}
-merge_strategy = input("Enter merge strategy ('first' or 'second'): ")
-result = merge_dicts(dict1, dict2, merge_strategy)
-print("Merged Dictionary:", result)
+while 1:
+    a=input("enter a key1 for dict1: ")
+    b=input("enter a key2 for dict1: ")
+    c=input("enter a value for dict1: ")
+    d=input("enter a value for dict1: ")
+    e=input("enter a key1 for dict2: ")
+    f=input("enter a key2 for dict2: ")
+    g=input("enter a value for key2: ")
+    h=input("enter a value for key2: ")
+    dict1 = {a: c, b: d}
+    dict2 = {e: g, f: h}
+    merge_strategy = input("Enter merge strategy ('first' or 'second'): ")
+    if dict1!=dict2:
+        result = merge_dicts(dict1, dict2, merge_strategy)
+        print("Merged Dictionary:", result)
+    else:
+        print("the dict are same!")
 
