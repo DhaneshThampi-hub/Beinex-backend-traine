f6b2d92d4ec65daca5a26e9e215097da4461782a
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-02-16 13:12:08 +0530

    Update dictq3.py

--- task10-feb16/dictq3.py
+++ task10-feb16/dictq3.py
@@ -1,10 +1,10 @@
 '''3.	Write a Python program to print a dictionary where the keys are numbers
  between 1 and 15 (both included) and the values are the square of the keys'''
 
-
-a=int(input("enter a  start limit: "))
-b=int(input("enter end limit: "))
-square_dict = {}
-for i in range(a, b+1):
-    square_dict[i] = i**2
-print(square_dict)
+while 1:
+    a=int(input("enter a  start limit: "))
+    b=int(input("enter end limit: "))
+    square_dict = {}
+    for i in range(a, b+1):
+        square_dict[i] = i**2
+    print(square_dict)
