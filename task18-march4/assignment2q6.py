0256bf0d21ebf4348df7f457432b764dfc3bc816
DhaneshThampi-hub <159514392+DhaneshThampi-hub@users.noreply.github.com>
2024-03-01 11:56:45 +0530

    Update assignment2q6.py

--- task18-march4/assignment2q6.py
+++ task18-march4/assignment2q6.py
@@ -1,6 +1,5 @@
-'''6. Write a Python function that takes a list of strings as input and returns a 
-new list with the strings sorted in descending order of their lengths. '''
-
+"""6. Write a Python function that takes a list of strings as input and returns a 
+new list with the strings sorted in descending order of their lengths. """
 
 
 def sort_strings_by_length(input_list):
@@ -15,7 +14,8 @@ def sort_strings_by_length(input_list):
     """
     return sorted(input_list, key=len, reverse=True)
 
+
 # Example usage:
-input_strings = input('enter a string: ').split(',')
+input_strings = input("enter a string: ").split(",")
 sorted_strings = sort_strings_by_length(input_strings)
 print(sorted_strings)
